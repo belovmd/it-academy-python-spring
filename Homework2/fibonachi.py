ead3933076ae94f63b6c1b516c2de5d9aae4a453
Ruslan <6441066@mail.ru>
2020-01-26 01:20:47 +0300

    My First Realize

--- Homework2/fibonachi.py
+++ Homework2/fibonachi.py
@@ -1,8 +1,8 @@
-n=int(input("Введите n:"))
-fibonachi_previous=0
-fibonachi_current=1
-for i in range(n-1):
-    fibonachi_save=fibonachi_current
-    fibonachi_current +=fibonachi_previous
-    fibonachi_previous=fibonachi_save
+n = int(input("Введите n:"))
+fibonachi_previous = 0
+fibonachi_current = 1
+for i in range(n - 1):
+    fibonachi_save = fibonachi_current
+    fibonachi_current += fibonachi_previous
+    fibonachi_previous = fibonachi_save
 print(fibonachi_previous)
--- Homework2/palindrom.py
+++ Homework2/palindrom.py
@@ -1,20 +1,20 @@
-number=int(input("Введите число:"))
-kol_number=0
-n=number
-while n>0:
-    n=n // 10
-    kol_number+=1
+number = int(input("Введите число:"))
+kol_number = 0
+n = number
+while n > 0:
+    n = n // 10
+    kol_number += 1
 # divide our number into two parts
-n=kol_number // 2
-kol_number-=1
+n = kol_number // 2
+kol_number -= 1
 # comparing first part of number with second part
-for i in range(1,n+1):
-    first_number=number // 10**kol_number
-    last_number=number % 10
+for i in range(1, n + 1):
+    first_number = number // 10 ** kol_number
+    last_number = number % 10
 
-    if  first_number!=last_number:
+    if first_number != last_number:
         print("Не палиндром.")
         break
-    number=number - first_number*10**kol_number - last_number
+    number = number - first_number * 10 ** kol_number - last_number
 else:
     print("Палиндром!!!")

cb059f3feb0ae81b5737051aee7c1c18c7b1eba2
unknown <alex_ugalek@mail.ru>
2020-01-27 01:15:18 +0300

    Homework2.3

--- src/Homework2/common_price.py
+++ src/Homework2/common_price.py
@@ -1,12 +1,7 @@
-try:
-    price_rubles_per_item = int(input('Enter part of price '
-                                      'per item in rubles: '))
-    price_cent_per_item = int(input('Enter part of price per item in cents: '))
-    numbers_of_items = int(input('Enter numbers of items: '))
-    common_price_cents = (price_cent_per_item * numbers_of_items)
-    common_price_rubles = ((price_rubles_per_item * numbers_of_items) +
-                           common_price_cents // 100)
-    print(f'Common price is: {common_price_rubles} rubles '
-          f'{common_price_cents % 100} cents')
-except ValueError as error:
-    print(error)
+pr_rubles_per_item = int(input('Enter part of price per item in rubles: '))
+pr_cent_per_item = int(input('Enter part of price per item in cents: '))
+num_of_items = int(input('Enter numbers of items: '))
+common_pr_c = (pr_cent_per_item * num_of_items)
+common_pr_r = ((pr_rubles_per_item * num_of_items) + common_pr_c // 100)
+print(f'Common price is: {common_pr_r} rubles '
+      f'{common_pr_c % 100} cents')

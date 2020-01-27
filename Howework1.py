b31f156ec073e717339cc86ff31bbdee43114f53
WizarDTM-IP <igor.potapovw@gmail.com>
2020-01-28 01:24:58 +0300

    Fixed code

--- Howework1.py
+++ Howework1.py
@@ -1,3 +1,8 @@
+import re
+import sys
+import glob
+from time import localtime
+
 # 1---------------------------------------------------------------------------------------------------------------------
 print('Hello, world!')
 # 2---------------------------------------------------------------------------------------------------------------------
@@ -20,3 +25,55 @@ def greet(names):
     greet('Jill')
     greet('Bob')
     print('Hello' + names)
+
+
+# 6---------------------------------------------------------------------------------------------------------------------
+for test_string in ['555-1212', 'ILL-EGAL']:
+    if re.match(r'^\d{3}-\d{4}$', test_string):
+        print(test_string, 'is a valid US local phone number')
+    else:
+        print(test_string, 'rejected')
+# 7---------------------------------------------------------------------------------------------------------------------
+prices = {'apple': 0.40, 'banana': 0.50}
+my_purchase = {
+    'apple': 1,
+    'banana': 6}
+grocery_bill = sum(prices[fruit] * my_purchase[fruit]
+                   for fruit in my_purchase)
+print('I owe the grocer $%.2f' % grocery_bill)
+# 8---------------------------------------------------------------------------------------------------------------------
+# This program adds up integers that have been passed as arguments in the command line
+try:
+    total = sum(int(arg) for arg in sys.argv[1:])
+    print('sum =', total)
+except ValueError:
+    print('Please supply integer arguments')
+# 9---------------------------------------------------------------------------------------------------------------------
+# indent your Python code to put into an email
+# glob supports Unix style pathname extensions
+python_files = glob.glob('*.py')
+for file_name in sorted(python_files):
+    print('    ------' + file_name)
+
+    with open(file_name) as f:
+        for line in f:
+            print('    ' + line.rstrip())
+
+    print()
+# 10--------------------------------------------------------------------------------------------------------------------
+activities = {8: 'Sleeping',
+              9: 'Commuting',
+              17: 'Working',
+              18: 'Commuting',
+              20: 'Eating',
+              22: 'Resting'}
+
+time_now = localtime()
+hour = time_now.tm_hour
+
+for activity_time in sorted(activities.keys()):
+    if hour < activity_time:
+        print(activities[activity_time])
+        break
+else:
+    print('Unknown, AFK or sleeping!')

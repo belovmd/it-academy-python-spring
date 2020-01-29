cf4d8502c2744d8d6a79c9334463739b1db94a43
WizarDTM-IP <igor.potapovw@gmail.com>
2020-01-30 01:18:36 +0300

    Fixed code V1.6

--- Homework1.py
+++ Homework1.py
@@ -1,7 +1,12 @@
+import csv
 import glob
+import itertools
+from itertools import groupby
+import random
 import re
 import sys
 from time import localtime
+import xml.etree.ElementTree as etree
 import unittest
 
 # 1 line: Output
@@ -21,6 +26,7 @@ parents, babies = (1, 1)
 while babies < 100:
     print('This generation has {0} babies'.format(babies))
     parents, babies = (babies, parents + babies)
+
 
 # 5 lines: Functions
 
@@ -145,11 +151,11 @@ if __name__ == '__main__':
 
 
 # 14 lines: Doctest-based testing
-def median(pools):
+def median(pool):
     # Statistical median to demonstrate doctest.
     # median([2, 9, 9, 7, 9, 2, 4, 5, 8])
     # 6 change to 7 in order to pass the test
-    copy = sorted(pools)
+    copy = sorted(pool)
     size = len(copy)
     if size % 2 == 1:
         return copy[int((size - 1) / 2)]
@@ -160,3 +166,188 @@ def median(pools):
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
+
+# 15 lines: itertools
+
+lines = '''
+This is the
+first paragraph.
+
+This is the second.
+'''.splitlines()
+# Use itertools.groupby and bool to return groups of
+# consecutive lines that either have content or don't.
+for has_chars, frags in groupby(lines, bool):
+    if has_chars:
+        print(' '.join(frags))
+
+
+# PRINTS:
+# This is the first paragraph.
+# This is the second.
+
+# 16 lines: csv module, tuple unpacking, cmp() built-in
+
+
+# need to define cmp function in Python 3
+def cmp(a, b):
+    return (a > b) - (a < b)
+
+
+# write stocks data as comma-separated values
+with open('stocks.csv', 'w', newline='') as stocksFileW:
+    writer = csv.writer(stocksFileW)
+    writer.writerows([
+        ['GOOG', 'Google, Inc.', 505.24, 0.47, 0.09],
+        ['YHOO', 'Yahoo! Inc.', 27.38, 0.33, 1.22],
+        ['CNET', 'CNET Networks, Inc.', 8.62, -0.13, -1.4901]
+    ])
+
+# read stocks data, print status messages
+with open('stocks.csv', 'r') as stocksFile:
+    stocks = csv.reader(stocksFile)
+
+    status_labels = {-1: 'down', 0: 'unchanged', 1: 'up'}
+    for ticker, name, price, change, pct in stocks:
+        status = status_labels[cmp(float(change), 0.0)]
+        print('%s is %s (%.2f)' % (name, status, float(pct)))
+
+# 18 lines: 8-Queens Problem (recursion)
+BOARD_SIZE = 8
+
+
+def under_attack(col, queenss):
+    left = right = col
+
+    for r, c in reversed(queenss):
+        left, right = left - 1, right + 1
+
+        if c in (left, col, right):
+            return True
+    return False
+
+
+def solve(n):
+    if n == 0:
+        return [[]]
+
+    smaller_solutions = solve(n - 1)
+
+    return [solution + [(n, i + 1)]
+            for g in range(BOARD_SIZE)
+            for solution in smaller_solutions
+
+            if not under_attack(g + 1, solution)]
+
+
+for answer in solve(BOARD_SIZE):
+    print(answer)
+
+
+# 20 lines: Prime numbers sieve w/fancy generators
+def iter_primes():
+    # an iterator of all numbers between 2 and +infinity
+    numbers = itertools.count(2)
+
+    # generate primes forever
+    while True:
+        # get the first number from the iterator (always a prime)
+        prime = next(numbers)
+        yield prime
+
+        # this code iteratively builds up a chain of
+        # filters...slightly tricky, but ponder it a bit
+        numbers = filter(prime.__rmod__, numbers)
+
+
+for p in iter_primes():
+    if p > 1000:
+        break
+    print(p)
+
+# 21 lines: XML/HTML parsing
+dinner_recipe = '''<html><body><table>
+<tr><th>amt</th><th>unit</th><th>item</th></tr>
+<tr><td>24</td><td>slices</td><td>baguette</td></tr>
+<tr><td>2+</td><td>tbsp</td><td>olive oil</td></tr>
+<tr><td>1</td><td>cup</td><td>tomatoes</td></tr>
+<tr><td>1</td><td>jar</td><td>pesto</td></tr>
+</table></body></html>'''
+
+# From http://effbot.org/zone/element-index.htm
+
+tree = etree.fromstring(dinner_recipe)
+
+# For invalid HTML use http://effbot.org/zone/element-soup.htm
+# import ElementSoup, StringIO
+# tree = ElementSoup.parse(StringIO.StringIO(dinner_recipe))
+
+pantry = {'olive oil', 'pesto'}
+for ingredient in tree.getiterator('tr'):
+    amt, unit, item = ingredient
+    if item.tag == "td" and item.text not in pantry:
+        print("%s: %s %s" % (item.text, amt.text, unit.text))
+
+# 28 lines: 8-Queens Problem (define your own exceptions)
+BOARD_SIZE = 8
+
+
+class BailOut(Exception):
+    pass
+
+
+def validate(queensss):
+    left = right = col = queensss[-1]
+    for r in reversed(queensss[:-1]):
+        left, right = left - 1, right + 1
+        if r in (left, col, right):
+            raise BailOut
+
+
+def add_queen(queensss, queens):
+    for y in range(BOARD_SIZE):
+        test_queens = queens + [y]
+        try:
+            validate(test_queens)
+            if len(test_queens) == BOARD_SIZE:
+                return test_queens
+            else:
+                return add_queen(test_queens)
+        except BailOut:
+            pass
+    raise BailOut
+
+
+queensss = add_queen([])
+print(queensss)
+print("\n".join(". " * q + "Q " + ". " * (BOARD_SIZE - q - 1) for q in queensss))
+
+# 33 lines: "Guess the Number" Game (edited) from http://inventwithpython.com
+
+
+guesses_made = 0
+
+name = input('Hello! What is your name?\n')
+
+number = random.randint(1, 20)
+print('Well, {0}, I am thinking of a number between 1 and 20.'.format(name))
+
+while guesses_made < 6:
+
+    guess = int(input('Take a guess: '))
+
+    guesses_made += 1
+
+    if guess < number:
+        print('Your guess is too low.')
+
+    if guess > number:
+        print('Your guess is too high.')
+
+    if guess == number:
+        break
+
+if guess == number:
+    print('Good job, {0}! You guessed my number in {1} guesses!'.format(name, guesses_made))
+else:
+    print('Nope. The number I was thinking of was {0}'.format(number))

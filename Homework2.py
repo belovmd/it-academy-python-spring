51f911bcfe29ed4967e2f120ab6f9f1f871c7483
WizarDTM-IP <igor.potapovw@gmail.com>
2020-01-29 21:53:15 +0300

    Homework2 V1

--- Homework2.py
+++ Homework2.py
@@ -3,15 +3,3 @@ MN = float(input("Введите цену:"))
 L = int(input("Введите количество"))
 total = str(MN * L)
 print("Общая цена " + str(int(MN + L)) + " рублей" + " " + total[-15:-13] + " копеек")
-
-# 3
-s = input()
-s = s.replace(",", "")
-s = s.replace("!", "")
-s = s.replace("?", "")
-s = s.replace(".", "")
-s = s.split()
-s.sort(key=len)
-print(s[-1])
-
-# 4

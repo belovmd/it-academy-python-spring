dc49217b46f166c94bd3428cd1f4ba8b9a16da19
JanGonza <alex.milan@mail.ru>
2020-01-29 21:02:08 +0300

    ver 1.3

--- src/Task2.py
+++ src/Task2.py
@@ -1,14 +1,19 @@
-"""You probably know the "like" system from Facebook and other pages. People can "like" blog posts,
-pictures or other items. We want to create the text that should be displayed next to such an item.
-Implement a function likes :: [String] -> String, which must take in input array,
-containing the names of people who like an item. It must return the display
-text as shown in the examples:
+"""You probably know the "like" system from Facebook and other pages.
+People can "like" blog posts,
+pictures or other items. We want to create the text
+that should be displayed next to such an item.
+Implement a function likes :: [String] -> String,
+which must take in input array,
+containing the names of people who like an item. It must
+return the display text as shown in the examples:
 
 likes [] // must be "no one likes this"
 likes ["Peter"] // must be "Peter likes this"
 likes ["Jacob", "Alex"] // must be "Jacob and Alex like this"
-likes ["Max", "John", "Mark"] // must be "Max, John and Mark like this"
-likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
+likes ["Max", "John", "Mark"] // must be "Max,
+John and Mark like this"
+likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex,
+Jacob and 2 others like this"
 
 For 4 or more names, the number in and 2 others simply increases.
 https://www.codewars.com/kata/5266876b8f4bf2da9b000362
@@ -25,4 +30,5 @@ def likes(names):
     elif len(names) == 3:
         return('%s, %s and %s like this' % (names[0], names[1], names[2]))
     else:
-        return('%s, %s and %s others like this' % (names[0], names[1], len(names) - 2))
+        return('%s, %s and %s others like this' % (names[0],
+                                                   names[1], len(names) - 2))

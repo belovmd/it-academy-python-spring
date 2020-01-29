15eb4da9edb222015f745a1db3fbe6baad0daa1c
WizarDTM-IP <igor.potapovw@gmail.com>
2020-01-29 22:13:58 +0300

    Homework2 V3

--- Homework2.py
+++ Homework2.py
@@ -5,3 +5,13 @@ total = str(MN * L)
 a = str(int(MN + L))
 b = total[-15:-13]
 print("Общая цена " + a + " рублей" + " " + b + " копеек")
+
+# 3
+s = input()
+s = s.replace(",", "")
+s = s.replace("!", "")
+s = s.replace("?", "")
+s = s.replace(".", "")
+s = s.split()
+s.sort(key=len)
+print(s[-1])

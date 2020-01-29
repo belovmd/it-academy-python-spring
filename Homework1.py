b12f7063b15b280e03020a9b528850c1ad7ef5c4
WizarDTM-IP <igor.potapovw@gmail.com>
2020-01-30 00:22:33 +0300

    Fixed code V1.4

--- Homework1.py
+++ Homework1.py
@@ -145,12 +145,12 @@ if __name__ == '__main__':
 
 
 # 14 lines: Doctest-based testing
-def median(pool):
+def median(pools):
     # Statistical median to demonstrate doctest.
     median([2, 9, 9, 7, 9, 2, 4, 5, 8])
     # 6 change to 7 in order to pass the test
 
-    copy = sorted(pool)
+    copy = sorted(pools)
     size = len(copy)
     if size % 2 == 1:
         return copy[int((size - 1) / 2)]

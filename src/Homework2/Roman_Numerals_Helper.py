060e1f555ccb7ba5034b92d5ccc55a624cbb7f70
Siarhei Dzmitryienka <dzmitrock87@gmail.com>
2020-02-04 23:33:26 +0300

    Homework2.v5 - fix codestyle flake8

--- src/Homework2/Roman_Numerals_Helper.py
+++ src/Homework2/Roman_Numerals_Helper.py
@@ -28,19 +28,19 @@ assert(RomanNumerals.from_roman('MMVIII'), 2008, 'MMVIII should == 2008')
 
 class RomanNumerals(object):
     roman_nums = {
-            'I': 1,
-            'IV': 4,
-            'V': 5,
-            'IX': 9,
-            'X': 10,
-            'XL': 40,
-            'L': 50,
-            'XC': 90,
-            'C': 100,
-            'CD': 400,
-            'D': 500,
-            'CM': 900,
-            'M': 1000,
+        'I': 1,
+        'IV': 4,
+        'V': 5,
+        'IX': 9,
+        'X': 10,
+        'XL': 40,
+        'L': 50,
+        'XC': 90,
+        'C': 100,
+        'CD': 400,
+        'D': 500,
+        'CM': 900,
+        'M': 1000,
     }
     @classmethod
     def to_roman(cls, n):

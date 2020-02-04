aa85351da9ba4769217b955fa08b9b371ed27aed
Siarhei Dzmitryienka <dzmitrock87@gmail.com>
2020-02-04 23:20:03 +0300

    Homework2.v5 add new files

---
+++ src/Homework2/Pyramid_Slide_Down.py
@@ -0,0 +1,77 @@
+"""Pyramid Slide Down.
+
+For example, let's consider the following problem. Imagine that you
+have a pyramid built of numbers, like this one here:
+
+   /3/
+  \7\ 4
+ 2 \4\ 6
+8 5 \9\ 3
+
+Here comes the task...
+Let's say that the 'slide down' is a sum of consecutive numbers
+from the top to the bottom of the pyramid. As you can see, the
+longest 'slide down' is 3 + 7 + 4 + 9 = 23
+Your task is to write a function longestSlideDown
+(in ruby: longest_slide_down) that takes a pyramid representation as
+argument and returns its' longest 'slide down'.
+
+For example,
+longestSlideDown([[3], [7, 4], [2, 4, 6], [8, 5, 9, 3]]) => 23
+
+By the way...
+My tests include some extraordinarily high pyramides so as you can guess,
+brute-force method is a bad idea unless you have a few centuries to waste.
+You must come up with something more clever than that.
+
+"""
+from functools import reduce
+
+
+def longest_slide_down(pyramid):
+    pyr = pyramid[1:]
+    while len(pyr) > 1:
+        main_list = list()
+        tmp_row = list()
+        for i_ind, item in enumerate(pyr[0]):
+            sum_elements = []
+            sum_1 = item + pyr[0 + 1][i_ind]
+            sum_2 = item + pyr[0 + 1][i_ind + 1]
+            sum_elements.append(sum_1)
+            sum_elements.append(sum_2)
+            tmp_row.append(sum_elements.copy())
+        adv_list = reduce(lambda a, b: a + b, tmp_row)
+        main_list.append(adv_list[0])
+        for i in range(1, len(adv_list), 2):
+            if i == len(adv_list) - 1:
+                main_list.append(adv_list[-1])
+                break
+            else:
+                main_list.append(max(adv_list[i], adv_list[i + 1]))
+        pyr[1] = main_list.copy()
+        pyr = pyr[1:]
+        main_list.clear()
+    return max(pyr[0]) + pyramid[0][0]
+
+
+if __name__ == '__main__':
+    C = [[3], [7, 4], [2, 4, 6], [8, 5, 9, 3]]
+    L = [
+        [75],
+        [95, 64],
+        [17, 47, 82],
+        [18, 35, 87, 10],
+        [20, 4, 82, 47, 65],
+        [19, 1, 23, 75, 3, 34],
+        [88, 2, 77, 73, 7, 63, 67],
+        [99, 65, 4, 28, 6, 16, 70, 92],
+        [41, 41, 26, 56, 83, 40, 80, 70, 33],
+        [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+        [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+        [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+        [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+        [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+        [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23],
+    ]
+    assert longest_slide_down(C) == 23
+    assert longest_slide_down(L) == 1074
---
+++ src/Homework2/Roman_Numerals_Helper.py
@@ -0,0 +1,75 @@
+"""Roman Numerals Helper.
+
+Task
+Create a RomanNumerals class that can convert a roman numeral to and from
+an integer value. It should follow the API demonstrated in the examples below.
+Multiple roman numeral values will be tested for each helper method.
+Modern Roman numerals are written by expressing each digit separately starting
+with the left most digit and skipping any digit with a value of zero. In Roman
+numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC.
+2008 is written as 2000=MM, 8=VIII; or MMVIII.
+1666 uses each Roman symbol in descending order: MDCLXVI.
+
+Examples
+RomanNumerals.to_roman(1000) # should return 'M'
+RomanNumerals.from_roman('M') # should return 1000
+Help
+| Symbol | Value |
+| I | 1 | | V | 5 | | X | 10 | | L | 50 | | C | 100 | | D | 500 | | M | 1000 |
+
+assert(RomanNumerals.to_roman(1000), 'M', '1000 should == "M"')
+assert(RomanNumerals.to_roman(1990), 'MCMXC', '1990 should == "MCMXC"')
+
+assert(RomanNumerals.from_roman('XXI'), 21, 'XXI should == 21')
+assert(RomanNumerals.from_roman('MMVIII'), 2008, 'MMVIII should == 2008')
+
+"""
+
+
+class RomanNumerals:
+    roman_nums = {
+            'I': 1,
+            'IV': 4,
+            'V': 5,
+            'IX': 9,
+            'X': 10,
+            'XL': 40,
+            'L': 50,
+            'XC': 90,
+            'C': 100,
+            'CD': 400,
+            'D': 500,
+            'CM': 900,
+            'M': 1000,
+    }
+    @classmethod
+    def to_roman(cls, n):
+        """Convert digit to RomanNumeral."""
+        romans = sorted(cls.roman_nums.items(), key=lambda i: i[1],
+                        reverse=True)
+        roman_num = ''
+        for r_num, value in romans:
+            roman_num += r_num * (n // value)
+            n -= value * (n // value)
+        return roman_num
+
+    @classmethod
+    def from_roman(cls, roman_num):
+        """Convert RomanNumeral to digit."""
+        digit_num = 0
+        while roman_num:
+            if roman_num[:2] in cls.roman_nums.keys():
+                digit_num += cls.roman_nums.get(roman_num[:2])
+                roman_num = roman_num[2:]
+            else:
+                digit_num += cls.roman_nums.get(roman_num[0])
+                roman_num = roman_num[1:]
+        return digit_num
+
+
+if __name__ == '__main__':
+    assert RomanNumerals.to_roman(1000) == 'M'
+    assert RomanNumerals.to_roman(1990) == 'MCMXC'
+
+    assert RomanNumerals.from_roman('XXI') == 21
+    assert RomanNumerals.from_roman('MMVIII') == 2008

907cca2968f0eb6e73218a1e1f33e391646741c4
Dmitry <dmitri.radykov@gmail.com>
2020-02-04 13:53:56 +0300

    Add homework1.py

---
+++ homework1.py
@@ -0,0 +1,13 @@
+print ('Hello, world')
+
+name = input('What is your name?\n')
+print ('Hi, %s.' % name)
+
+friends = ['john', 'pat', 'gary', 'michael']
+for i, name in enumerate(friends):
+    print ("iteration {iteration} is {name}".format(iteration=i, name=name))
+
+parents, babies = (1, 1)
+while babies < 100:
+    print ('This generation has {0} babies'.format(babies))
+    parents, babies = (babies, parents + babies)

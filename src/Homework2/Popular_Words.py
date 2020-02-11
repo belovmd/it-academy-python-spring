d9dcbd16f09d5e2ede77522832f2bc43c3a7a5c3
Siarhei Dzmitryienka <dzmitrock87@gmail.com>
2020-02-11 22:30:27 +0300

    Homework2.v10 fix code style PEP8

--- src/Homework2/Popular_Words.py
+++ src/Homework2/Popular_Words.py
@@ -26,12 +26,9 @@ def popular_words(text: str, words: list) -> dict:
 
 
 if __name__ == '__main__':
-    assert popular_words('''
-    When I was One 
-    I had just begun
-    When I was Two
-    I was nearly new
-    ''', ['i', 'was', 'three', 'near']) == {'i': 4,
-                                            'was': 3,
-                                            'three': 0,
-                                            'near': 0}
+    assert popular_words('When I was One I had just begun '
+                         'When I was Two I was nearly new',
+                         ['i', 'was', 'three', 'near']) == {'i': 4,
+                                                            'was': 3,
+                                                            'three': 0,
+                                                            'near': 0}

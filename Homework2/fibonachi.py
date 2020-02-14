360ae49793ea2fb1e5ede4c39c9eb0c93b152517
Ruslan Mazouka <6441066@mail.ru>
2020-02-14 18:59:34 +0300

    Homework 2 without Homework1 + corrected fibonachi

--- Homework2/fibonachi.py
+++ Homework2/fibonachi.py
@@ -2,8 +2,10 @@
 n = int(input("Введите n:"))
 fibonachi_previous = 0
 fibonachi_current = 1
-for i in range(n - 1):
-    fibonachi_save = fibonachi_current
-    fibonachi_current += fibonachi_previous
-    fibonachi_previous = fibonachi_save
+for i in range(n-1):
+    # fibonachi_save = fibonachi_current
+    # fibonachi_current += fibonachi_previous
+    # fibonachi_previous = fibonachi_save
+    fibonachi_previous, fibonachi_current = fibonachi_current, fibonachi_current + fibonachi_previous
+
 print(fibonachi_previous)

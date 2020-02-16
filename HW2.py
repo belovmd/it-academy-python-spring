fd13e0615e25aeb01488138b795d91ebbd4cc744
WizarDTM-IP <igor.potapovw@gmail.com>
2020-02-16 22:40:27 +0300

    Added string in #3

--- HW2.py
+++ HW2.py
@@ -1,3 +1,6 @@
+# for #3
+import string
+
 # 1
 MN = input("Введите цену:")
 MN = float(MN.replace(",", "."))
@@ -8,18 +11,14 @@ MN_PEN = int((total - int(total)) * 100)
 print("Общая цена {} рублей {} копеек".format(MN_RUB, MN_PEN))
 
 # 3
-s = input()
-s = s.replace(",", "")
-s = s.replace("!", "")
-s = s.replace("?", "")
-s = s.replace(".", "")
-s = s.split()
-s.sort(key=len)
-print(s[-1])
+my_str = input()
+my_punc = str.maketrans(dict.fromkeys(string.punctuation))
+my_word = my_str.translate(my_punc).split()
+my_word.sort(key=len)
+print(my_word[-1])
 
 # 4
 string = input()
-print(string)
 string_n = ''
 for i in range(len(string)):
     if string_n.find(string[i]) == -1 and string[i] != ' ':

89a69d620e3ef913948f86dbce53460298059578
Andrei Bartosh <sharp.0000ff@gmail.com>
2020-02-23 16:53:04 +0300

    release 0.2.1

--- src/21_line.py
+++ src/21_line.py
@@ -1,4 +1,7 @@
 # 21 lines: XML/HTML parsing
+# From http://effbot.org/zone/element-index.htm
+import xml.etree.ElementTree as etree
+
 dinner_recipe = '''<html><body><table>
 <tr><th>amt</th><th>unit</th><th>item</th></tr>
 <tr><td>24</td><td>slices</td><td>baguette</td></tr>
@@ -6,9 +9,6 @@ dinner_recipe = '''<html><body><table>
 <tr><td>1</td><td>cup</td><td>tomatoes</td></tr>
 <tr><td>1</td><td>jar</td><td>pesto</td></tr>
 </table></body></html>'''
-
-# From http://effbot.org/zone/element-index.htm
-import xml.etree.ElementTree as etree
 
 tree = etree.fromstring(dinner_recipe)
 

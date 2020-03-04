427e41828c765d65fee8f8bf4ce91bac8a744285
WizarDTM-IP <igor.potapovw@gmail.com>
2020-03-04 21:36:33 +0300

    Added str

--- HW2.py
+++ HW2.py
@@ -1,6 +1,7 @@
 # for №3
 import string
 
+
 # 1
 MN = input("Введите цену:")
 MN = float(MN.replace(",", "."))
@@ -10,12 +11,14 @@ MN_RUB = int(total)
 MN_PEN = int((total - int(total)) * 100)
 print("Общая цена {} рублей {} копеек".format(MN_RUB, MN_PEN))
 
+
 # 3
 my_str = input()
 my_punc = str.maketrans(dict.fromkeys(string.punctuation))
 my_word = my_str.translate(my_punc).split()
 my_word.sort(key=len)
 print(my_word[-1])
+
 
 # 4
 string = input()
@@ -24,6 +27,7 @@ for i in range(len(string)):
     if string_n.find(string[i]) == -1 and string[i] != ' ':
         string_n += string[i]
 print(string_n)
+
 
 # 5
 my_str = input()
@@ -37,6 +41,7 @@ for let in my_str:
 for k in catalog:
     print(k, ':', catalog[k])
 
+
 # n-th Fibonacci number
 fib1 = fib2 = 1
 n = int(input())
@@ -47,6 +52,7 @@ print(fib2, end=' ')
 for i in range(2, n):
     fib1, fib2 = fib2, fib1 + fib2
     print(fib2, end=' ')
+
 
 # Palindrome (True or False)
 num_pal = input()

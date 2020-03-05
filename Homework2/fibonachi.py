4eb2f5162d7566dd0eeb9408ce911345e527b3bb
Ruslan Mazouka <6441066@mail.ru>
2020-03-05 10:39:57 +0300

    Homework 2 after corrections:

--- Homework2/fibonachi.py
+++ Homework2/fibonachi.py
@@ -3,9 +3,6 @@ n = int(input("Введите n:"))
 f_prev = 0
 f_curr = 1
 for i in range(n - 1):
-    # fibonachi_save = fibonachi_current
-    # fibonachi_current += fibonachi_previous
-    # fibonachi_previous = fibonachi_save
     f_prev, f_curr = f_curr, f_curr + f_prev
 
 print(f_prev)
--- Homework2/palindrom.py
+++ Homework2/palindrom.py
@@ -5,14 +5,10 @@ n = number
 while n > 0:
     n = n // 10
     kol_number += 1
-# divide our number into two parts
-# n = kol_number // 2
-# kol_number -= 1
 # comparing first part of number with second part, first digit with last digit
 for i in range(1, kol_number + 1, 2):
     first_number = number // 10 ** (kol_number - i)
     last_number = number % 10
-
     if first_number != last_number:
         print("Не палиндром.")
         break

b3b53f2f0481475335fc4e2fce1ae768fa08a7db
alexugalek <alex_ugalek@mail.ru>
2020-03-09 09:42:12 +0300

    Remove extra conditions from nod.py

--- src/nod.py
+++ src/nod.py
@@ -4,9 +4,7 @@ a: integer
 b: integer
 greatest common factor of a and b
 """
-
 a, b = int(input('a = ')), int(input('b = '))
-a, b = max(a, b), min(a, b)
-while a % b != 0:
+while b != 0:
     a, b = b, a % b
-print(b)
+print(a)

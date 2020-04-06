0774da8a0ec289350eb22b8311bc4d6abd35701f
YuliyaGolovach <60254123+YuliyaGolovach@users.noreply.github.com>
2020-04-06 15:56:09 +0300

    Address review note #2
    Change solution to use intersection and union of sets.

--- src/hw_4/t05_pupils_and_languages.py
+++ src/hw_4/t05_pupils_and_languages.py
@@ -31,34 +31,19 @@ List of these languages.
 
 
 journal = {}
-languages = set()
-print('Input N - pupils number:')
-n = int(input())
-for i in range(n):
-    print('Input number of languages for pupil #{}:'.format(i + 1))
-    m = int(input())
-    pupils_languages = set()
-    for _ in range(m):
-        language = input()
-        pupils_languages.add(language)
-        languages.add(language)
-    journal[i] = pupils_languages
+# languages = set()
 
-popular_languages = []
-non_popular_languages = []
+with open('t05_input.txt', 'r') as file:
+    pupils_count = int(file.readline())
+    for i in range(pupils_count):
+        languages_count = int(file.readline())
+        pupils_languages = set()
+        for _ in range(languages_count):
+            pupils_languages.add(file.readline().strip())
+        journal[i] = pupils_languages
 
-for language in languages:
-    is_everybody_knows = True
-    is_at_least_one_knows = False
-    for lang_list in journal.values():
-        if language in lang_list:
-            is_at_least_one_knows = True
-        else:
-            is_everybody_knows = False
-    if is_everybody_knows:
-        popular_languages.append(language)
-    if is_at_least_one_knows:
-        non_popular_languages.append(language)
+popular_languages = set.intersection(*journal.values())
+non_popular_languages = set.union(*journal.values())
 
 print(len(popular_languages))
 for language in popular_languages:

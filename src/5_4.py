b38c8b630a43f5113113c8820f2b0e1e6fe9f7e0
Sankouski Maxim <sankouski.m@gmail.com>
2020-04-07 14:37:04 +0300

    Edit line breaks

--- src/5_4.py
+++ src/5_4.py
@@ -62,10 +62,10 @@ dct_years = {element: list_years.count(element)
 
 with open('ratings.txt', 'w') as file:
     for element in dct_ratings:
-        file.write(element + ' : ' +
-                   str(dct_ratings[element]) + '\n')
+        file.write(element + ' : '
+                   + str(dct_ratings[element]) + '\n')
 
 with open('years.txt', 'w') as file:
     for element in dct_years:
-        file.write(element + ' : ' +
-                   str(dct_years[element]) + '\n')
+        file.write(element + ' : '
+                   + str(dct_years[element]) + '\n')

e579b06f6b9b863dd8975ddcff4d7a2cf2035bb6
s.dzmitryienka <s.dzmitryienka@exaft.com>
2020-04-23 23:22:27 +0300

    task 2 complete

---
+++ src/Homework6/2_decorator.py
@@ -0,0 +1,73 @@
+"""2 - decorator
+Создайте декоратор, который вызывает задекорированную функцию пока она не
+выполнится без исключений (но не более n раз - параметр декоратора). Если
+превышено количество попыток, должно быть возбуждено исключение типа
+TooManyErrors
+
+"""
+import unittest
+import logging
+from functools import wraps
+
+logging.basicConfig(
+     level=logging.INFO,
+     format="%(asctime)s | %(levelname)s | %(filename)s:%(lineno)d %(message)s"
+ )
+logger = logging.getLogger(__name__)
+
+
+class TooManyErrors(Exception):
+    def __init__(self, obj_name):
+        super().__init__()
+        self.obj_name = obj_name
+
+    def __str__(self):
+        return f'{self.obj_name} raise exception TooManyErrors'
+
+
+def decor_runner(n: int):
+    counter = 0
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(x, y):
+            nonlocal counter
+            while True:
+                if counter >= n:
+                    logger.warning(f'Limit trying count. Counts = {counter}')
+                    counter = 0  # reset counter for unittests
+                    raise TooManyErrors(func.__name__)
+                func(x, y)
+                y -= 1
+                logger.info(f'{func.__name__} was running {counter} times')
+                counter += 1
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+
+@decor_runner(3)
+def divide_numbers(x: int or float, y: int) -> float:
+    try:
+        result = x / y
+        logger.info(f'divide_numbers({x}, {y}) => {result}')
+    except ZeroDivisionError as ex:
+        logger.warning(f'divide_numbers({x}, {y}): {ex}')
+        raise ZeroDivisionError
+    return result
+
+
+class TestDecoratorRunFunction(unittest.TestCase):
+    def test_exception_too_many_errors(self):
+        logger.info('test__1')
+        with self.assertRaises(TooManyErrors):
+            divide_numbers(3, 5)
+
+    def test_exception_zero_division(self):
+        logger.info('test__2')
+        with self.assertRaises(ZeroDivisionError):
+            divide_numbers(3, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
